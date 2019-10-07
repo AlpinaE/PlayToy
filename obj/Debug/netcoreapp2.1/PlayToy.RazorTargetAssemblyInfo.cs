b1cc3025f9da7b4f988327272a274dd53ad88582
Alpina <alpina_echevarria@usmp.pe>
2019-10-06 20:12:06 -0500

    1

--- obj/Debug/netcoreapp2.1/PlayToy.RazorTargetAssemblyInfo.cs
+++ obj/Debug/netcoreapp2.1/PlayToy.RazorTargetAssemblyInfo.cs
@@ -7,10 +7,12 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.CompiledRazorAssemblyApplicationPartFac" +
+"tory, Microsoft.AspNetCore.Mvc.Razor")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayToy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyProductAttribute("PlayToy")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("PlayToy")]
-[assembly: System.Reflection.AssemblyTitleAttribute("PlayToy")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PlayToy.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
