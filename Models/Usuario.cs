f5bd46c4e5d79df4cda3568945145dfd47cb04fb
Alpina <alpina_echevarria@usmp.pe>
2019-10-19 00:23:02 -0500

    Registrar

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PlayToy_3.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string ApePat { get; set; }
+        public string ApeMat { get; set; }
+        public DateTime FecNac { get; set; }
         public string Correo { get; set; }
+        public string Contrasena { get; set; }
     }
 }
