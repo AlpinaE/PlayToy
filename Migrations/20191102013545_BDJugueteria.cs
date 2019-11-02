992e0f12761ee7268fe6c36b7d18aa332891cb2f
Alpina <alpina_echevarria@usmp.pe>
2019-11-01 21:44:18 -0500

    migrations

---
+++ Migrations/20191102013545_BDJugueteria.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PlayToy.Migrations
+{
+    public partial class BDJugueteria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Juguetes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Nombre = table.Column<string>(nullable: true),
+                    Precio = table.Column<decimal>(nullable: false),
+                    Foto = table.Column<string>(nullable: true),
+                    Categoria = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Juguetes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    User = table.Column<string>(nullable: true),
+                    Nombre = table.Column<string>(nullable: true),
+                    ApePat = table.Column<string>(nullable: true),
+                    ApeMat = table.Column<string>(nullable: true),
+                    FecNac = table.Column<DateTime>(nullable: false),
+                    Correo = table.Column<string>(nullable: true),
+                    Contrasena = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    fecha = table.Column<DateTime>(nullable: false),
+                    IDUsuario = table.Column<int>(nullable: true),
+                    UsuarioId = table.Column<int>(nullable: true),
+                    IDJuguete = table.Column<int>(nullable: true),
+                    JugueteID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Pedidos_Juguetes_JugueteID",
+                        column: x => x.JugueteID,
+                        principalTable: "Juguetes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Pedidos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_JugueteID",
+                table: "Pedidos",
+                column: "JugueteID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_UsuarioId",
+                table: "Pedidos",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+
+            migrationBuilder.DropTable(
+                name: "Juguetes");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
